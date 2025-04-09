cb85f3c49c4f8711004385897a5406ddfe3dff43
Jose Felipe Almeida dos Santos <80927089+Josefelipedev@users.noreply.github.com>
2025-04-09 09:26:55 -0300

    feat:table e seachForm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { HeaderContainer, HeaderContent } from './styles.ts';
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton,
+} from './styles.ts';
 import logoIgnite from '../../assets/logo-ignite.svg';
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoIgnite} alt="logo ignite" />
+        <NewTransactionButton>Nova Transação </NewTransactionButton>
       </HeaderContent>
     </HeaderContainer>
   );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,40 @@
 import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles.ts';
+import { SearchForm } from './components/SearchForm';
 
 export function Transactions() {
   return (
     <div>
-      {' '}
       <Header />
+      <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td className="deposit">
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>20/01/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel</td>
+              <td className="withdraw">
+                <PriceHighlight variant="outcome"> -R$ 1.000,00</PriceHighlight>
+              </td>
+              <td>Casa</td>
+              <td>20/01/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
